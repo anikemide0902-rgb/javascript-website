ba34b037d300fd4c11b778284d075541e0847f40
anikemide0902-rgbecho <anikemide0902@gmail.com>
2026-02-23 03:46:44 +0100

    responsiveness

--- script2.js
+++ script2.js
@@ -1,36 +1,36 @@
-// var userName = prompt('What is your name?');
+var userName = prompt('What is your name?');
 
-// //selectors (getElementById, getElementByClassName, getElementByTagName, querySelector, querySelectorAll)
+//selectors (getElementById, getElementByClassName, getElementByTagName, querySelector, querySelectorAll)
 
-// var header = document.querySelector('.header');
+var header = document.querySelector('.header');
 
-// header.innerText = `Hello ${userName}, welcome to javascript website`;
+header.innerText = `Hello ${userName}, welcome to javascript website`;
 
-// var btnReset = document.querySelector('.btn-solid');
+var btnReset = document.querySelector('.btn-solid');
 
-// btnReset.addEventListener("click", function () {
-//     alert('You have clicked me');
-// });
+btnReset.addEventListener("click", function () {
+    alert('You have clicked me');
+});
 
-// var redBtn = document.querySelector('.red');
-// var centreCard = document.querySelector('.centered-card');
+var redBtn = document.querySelector('.red');
+var centreCard = document.querySelector('.centered-card');
 
-// redBtn.addEventListener("click", function() {
-//     centreCard.style.backgroundColor = "rgb(184, 16, 16)";
-// });
-
-
-// var blueBtn = document.querySelector('.blue');
-// var centerCard = document.querySelector('.centered-card');
-
-// blueBtn.addEventListener("click", function() {
-//     centerCard.style.backgroundColor = "rgb(16, 16, 184)";
-// });
+redBtn.addEventListener("click", function() {
+    centreCard.style.backgroundColor = "rgb(184, 16, 16)";
+});
 
 
-// var greenBtn = document.querySelector('.green');
-// var centeCard = document.querySelector('.centered-card');
+var blueBtn = document.querySelector('.blue');
+var centerCard = document.querySelector('.centered-card');
 
-// greenBtn.addEventListener("click", function() {
-//     centeCard.style.backgroundColor = "rgb(16, 184, 16)";
-// });
+blueBtn.addEventListener("click", function() {
+    centerCard.style.backgroundColor = "rgb(16, 16, 184)";
+});
+
+
+var greenBtn = document.querySelector('.green');
+var centeCard = document.querySelector('.centered-card');
+
+greenBtn.addEventListener("click", function() {
+    centeCard.style.backgroundColor = "rgb(16, 184, 16)";
+});
